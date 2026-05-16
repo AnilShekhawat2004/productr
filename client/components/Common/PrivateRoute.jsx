9710a7c6a49e68950348e51a44099257589b4ef8
AnilShekhawat2004 <anilsinghshekhawat9672@gmail.com>
2026-05-16 19:17:35 +0530

    Logout api naming error

--- client/components/Common/PrivateRoute.jsx
+++ client/components/Common/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import { jwtDecode } from "jwt-decode";
 import { useSelector, useDispatch } from "react-redux";
 import { Navigate } from "react-router-dom";
-import { logout } from "../../services/operations/authAPI";
+import { logoutUser } from "../../services/operations/authAPI";
 
 const PrivateRoute = ({ children }) => {
   const { token, loading } = useSelector((state) => state.auth);
@@ -24,12 +24,12 @@ const PrivateRoute = ({ children }) => {
     const currentTime = Date.now() / 1000;
 
     if (decoded.exp < currentTime) {
-      dispatch(logout());
+      dispatch(logoutUser());
       return <Navigate to="/login" replace />;
     }
   } catch (error) {
     console.log("Invalid Token", error);
-    dispatch(logout());
+    dispatch(logoutUser());
     return <Navigate to="/login" replace />;
   }
 
