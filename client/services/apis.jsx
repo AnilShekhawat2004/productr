7ede9faf595bd3b12d4bd644d0e2399eafe79b76
AnilShekhawat2004 <anilsinghshekhawat9672@gmail.com>
2026-05-16 14:41:13 +0530

    Update api connection

--- client/services/apis.jsx
+++ client/services/apis.jsx
@@ -2,19 +2,19 @@ const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 //Auth endpoints
 export const authEndpoints = {
-  SIGNUP_API: BASE_URL + "/auth/signup",
-  LOGIN_API: BASE_URL + "/auth/login",
-  RESEND_OTP_API: BASE_URL + "/auth/resendOtp",
-  VERIFY_OTP_API: BASE_URL + "/auth/verifyOtp",
+  SIGNUP_API: BASE_URL + "/api/v1/auth/signup",
+  LOGIN_API: BASE_URL + "/api/v1/auth/login",
+  RESEND_OTP_API: BASE_URL + "/api/v1/auth/resendOtp",
+  VERIFY_OTP_API: BASE_URL + "/api/v1/auth/verifyOtp",
 };
 
 //Product endpoints
 export const productEndpoints = {
-  CREATE_PRODUCT_API: BASE_URL + "/product/createProduct",
-  EDIT_PRODUCT_API: BASE_URL + "/product/editProduct",
-  DELETE_PRODUCT_API: BASE_URL + "/product/deleteProduct",
-  GET_ALL_PRODUCTS_API: BASE_URL + "/product/getAllProducts",
-  GET_PRODUCT_DETAILS_API: BASE_URL + "/product/getProductDetails",
-  GET_ALL_STATUS_PRODUCTS_API: BASE_URL + "/product/getAllStatusProducts",
-  TOGGLE_STATUS_API: BASE_URL + "/product/toggleStatus",
+  CREATE_PRODUCT_API: BASE_URL + "/api/v1/product/createProduct",
+  EDIT_PRODUCT_API: BASE_URL + "/api/v1/product/editProduct",
+  DELETE_PRODUCT_API: BASE_URL + "/api/v1/product/deleteProduct",
+  GET_ALL_PRODUCTS_API: BASE_URL + "/api/v1/product/getAllProducts",
+  GET_PRODUCT_DETAILS_API: BASE_URL + "/api/v1/product/getProductDetails",
+  GET_ALL_STATUS_PRODUCTS_API: BASE_URL + "/api/v1/product/getAllStatusProducts",
+  TOGGLE_STATUS_API: BASE_URL + "/api/v1/product/toggleStatus",
 };
